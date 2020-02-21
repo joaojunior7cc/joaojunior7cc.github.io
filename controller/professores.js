c2e323bd30aae87052e608aab0e82d227b4a76b0
João Júnior <joao.junior7cc@gmail.com>
2020-02-21 19:15:38 -0300

    empty

--- controller/professores.js
+++
@@ -1,9 +0,0 @@
-var professoresModulo = angular.module('professoresModulo',[]);
-
-professoresModulo.controller("professoresController",function ($scope){
-    $scope.professores = [
-        {codigo:1, nome: 'João Júnior', email: 'jjr@email.com', fone: '11 999884455'},
-        {codigo:2, nome: 'Júnior João', email: 'jjr2@email.com', fone: '11 977884455'}
-
-    ];
-});
