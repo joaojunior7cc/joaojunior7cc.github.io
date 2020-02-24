dc06e7db0b37d7e2d177d10e6e40b2c8ea3009ce
João Júnior <joao.junior7cc@gmail.com>
2020-02-24 15:00:17 -0300

    Finalizado curriculo dinamico

--- js/controllers/homeJSONCtrl.js
+++ js/controllers/homeJSONCtrl.js
@@ -2,17 +2,24 @@
 angular.module("mygithub").controller("homeJSONCtrl",function ($scope,usuariosAPIService,serialGenerator) {
     $scope.usuarios = [];
     $scope.usuario = [];  
+    $scope.curriculos = [];
+    $scope.curriculo = [];
     $scope.corbh = "verde";
     
     console.log("controller");
     //dados JSON
     var carregarUsuarios = function () {
         usuariosAPIService.getUsuariosJSON().then(function(response) {
+            i = 0;
             console.log('GET OK ');
             $scope.usuarios = response.data.usuarios;
-            $scope.usuario = $scope.usuarios[0];
+            $scope.usuario = $scope.usuarios[i];
+            $scope.curriculos = response.data.curriculos;
+            $scope.curriculo = $scope.curriculos[i];
             //console.log($scope.usuarios);
-            console.log($scope.usuario);
+            console.log($scope.experiencias);
+            console.log($scope.curriculo);
+            //console.log($scope.usuario);
         },function (e) {
             $scope.err = true;
             $scope.erro = "Algo deu errado ["+$scope.err+"] no GET: "+ e;
