24d3a3d047bc53307e39952bb60b809fa8d80ab8
Joao Junior <joao.junior7cc@gmail.com>
2020-03-31 11:21:35 -0300

    ajuste para back API JSON

--- js/value/configValue.js
+++ js/value/configValue.js
@@ -1,7 +1,7 @@
 /*Tenho a variação de value: constant pode ser injetado em serviços do tipo provider   */
 //angular.module("listaTelefonica").value("config",{
 angular.module("mygithub").constant("configValue",{
-   baseUrlBack: "http://localhost:3412",
+   //baseUrlBack: "http://localhost:3412",
    //baseUrlJSON : "js/value/",
    baseUrlJSON: "http://joaojunio.sytes.net/slim/github"
    //baseUrlJSON: "http://localhost/slim/github"
